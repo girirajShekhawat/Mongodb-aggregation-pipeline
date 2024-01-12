e7bd000eec2bb2f1e8d1d8fdfd6ce88ec5005d45
Giriraj Singh Shekhawat <giriraj3103@gmail.com>
2024-01-12 14:17:58 +0530

    Q-1 completed

---
+++ data.js
@@ -0,0 +1,62 @@
+const  users=[
+    {
+      "name": "Alice",
+      "age": 25,
+      "favoriteColor": "Blue",
+      "favoriteFruit": "Apple"
+    },
+    {
+      "name": "Bob",
+      "age": 32,
+      "favoriteColor": "Green",
+      "favoriteFruit": "Kiwi"
+    },
+    {
+      "name": "Charlie",
+      "age": 18,
+      "favoriteColor": "Yellow",
+      "favoriteFruit": "Banana"
+    },
+    {
+      "name": "Diana",
+      "age": 41,
+      "favoriteColor": "Purple",
+      "favoriteFruit": "Grape"
+    },
+    {
+      "name": "Edward",
+      "age": 57,
+      "favoriteColor": "Red",
+      "favoriteFruit": "Strawberry"
+    },
+    {
+      "name": "Fatima",
+      "age": 15,
+      "favoriteColor": "Orange",
+      "favoriteFruit": "Mango"
+    },
+    {
+      "name": "George",
+      "age": 63,
+      "favoriteColor": "Brown",
+      "favoriteFruit": "Coffee Beans"
+    },
+    {
+      "name": "Helen",
+      "age": 28,
+      "favoriteColor": "Pink",
+      "favoriteFruit": "Watermelon"
+    },
+    {
+      "name": "Isaac",
+      "age": 7,
+      "favoriteColor": "Indigo",
+      "favoriteFruit": "Blueberries"
+    },
+    {
+      "name": "Jasmine",
+      "age": 39,
+      "favoriteColor": "Black",
+      "favoriteFruit": "Blackberries"
+    }
+  ]
